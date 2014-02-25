03cf56dc7bf40b840005cb0319e364be07a3c88e
Erik Edrosa <erik.edrosa@gmail.com>
2014-02-25 13:28:43 -0500

    Add first unit test

---
+++ test/test_lexer.py
@@ -0,0 +1,13 @@
+import unittest
+from src import lexer
+
+class TestLexerTokens(unittest.TestCase):
+  def setUp(self):
+    self.scanner = lexer.Lexer()
+
+  def test_int_literal(self):
+    # check for integer literals
+    self.scanner.scan("2")
+    token = self.scanner.token()
+    self.assertEqual(token.type, "INTLIT")
+
