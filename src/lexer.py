e47ecbee8f67b0b31826e21f853e99e57f03a0ee
Erik Edrosa <erik.edrosa@gmail.com>
2014-02-25 13:28:01 -0500

    Change token name to tokenSpec to remove conflict with python token module

--- src/lexer.py
+++ src/lexer.py
@@ -1,15 +1,16 @@
 import ply.lex as lex
-import token
+from src import tokenSpec
 
 class Lexer:
   def __init__(self):
     self.build()
 
   def build(self, **kwargs):
-    self.lexer = lex.lex(module=token, **kwargs)
+    self.lexer = lex.lex(module=tokenSpec, **kwargs)
 
   def scan(self, data):
     self.lexer.input(data)
 
   def token(self):
     return self.lexer.token()
+
