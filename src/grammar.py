81715bae7fb392267f6bc034cbd0667aec74f8e8
Erik Edrosa <erik.edrosa@gmail.com>
2014-03-05 13:40:41 -0500

    Add some grammar rules

--- src/grammar.py
+++ src/grammar.py
@@ -9,5 +9,29 @@ precedence = (
     ('right', 'PIPE')
 )
 
-def_
+def p_var_declaration(t):
+  'var_declaration : type ID'
+  #t[0] = TODO Some AST node
 
+def p_function_declaration(t):
+  'function_declaration : type ID param_list block'
+  #t[0] = TODO
+
+def p_type(t):
+  '''type : VOID
+          | INT
+          | FLOAT
+          | STRING
+          | BOOLEAN
+          '''
+  #t[0] = TODO
+
+def p_param_list(t):
+  '''param_list : var_declaration COMMA param_list
+                | var_declaration
+                '''
+  #t[0] = TODO
+
+def p_class_declaration(t):
+  'class_declaration : CLASS ID block'
+  #t[0] = TODO
