2ed8830a70de760081b5bf86ed763a6e9e0776b5
Erik Edrosa <erik.edrosa@gmail.com>
2014-03-17 15:54:18 -0400

    Fix lexer test cases

--- test/test_lexer.py
+++ test/test_lexer.py
@@ -26,28 +26,31 @@ class TestLexerTokens(unittest.TestCase):
       self.scanner.scan(int_lit)
       token = self.scanner.token()
       self.assertEqual(token.type, "INTLIT")
-
+      self.assertEqual(token.value, int(int_lit))
+ 
+  float_tokens = [
+      "0.1",
+      "3.14",
+      ".9634523"
+      ]
   def test_float_literal(self):
-    self.scanner.scan("0.1")
-    token = self.scanner.token()
-    self.assertEqual(token.type, "FLOATLIT")
-    self.scanner.scan("3.14")
-    token = self.scanner.token()
-    self.assertEqual(token.type, "FLOATLIT")
-    self.scanner.scan(".9634523")
-    token = self.scanner.token()
-    self.assertEqual(token.type, "FLOATLIT")
-
+    for float_lit in self.float_tokens:
+      self.scanner.scan(float_lit)
+      token = self.scanner.token()
+      self.assertEqual(token.type, "FLOATLIT")
+      self.assertEqual(token.value, float(float_lit))
   
+  string_tokens = [
+      '""',
+      '"Hello World!"',
+      '" \\" "'
+      ]
   def test_string_literal(self):
-    self.scanner.scan('""')
-    token = self.scanner.token()
-    self.assertEqual(token.type, "STRLIT")
-    self.assertEqual(token.value, "")
-    self.scanner.scan('"Hello World!"')
-    token = self.scanner.token()
-    self.assertEqual(token.type, "STRLIT")
-    self.assertEqual(token.value, "Hello World!")
+    for string_lit in self.string_tokens:
+      self.scanner.scan(string_lit)
+      token = self.scanner.token()
+      self.assertEqual(token.type, "STRLIT")
+      self.assertEqual(token.value, string_lit[1:-1])
   
   identifiers = [
       "foo",
