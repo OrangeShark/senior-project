f3a24fa92d58661deeee6e2a089091c219f39776
Erik Edrosa <erik.edrosa@gmail.com>
2014-04-24 21:17:38 -0400

    Fix setup.py to correctly install cnc Add a LICENSE Update .gitignore for Python eggs

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='cn',
-    version='0.1',
-    author='Erik Edrosa & Leo Shao',
-    scripts=['src/src.py'],
-    install_requires=['ply'],
+setup(name = 'cnc',
+    description = 'A compiler for the C natural programming language',
+    url = 'https://github.com/OrangeShark/senior-project',
+    license = 'GPL',
+    version = '0.1',
+    author = 'Erik Edrosa & Leo Shao',
+    author_email = 'eedro001@fiu.edu, lshao002@fiu.edu',
+    packages = find_packages(),
+    scripts = ['cnc.py'],
+    install_requires = ['ply'],
     entry_points = {
       'console_scripts': [
-        'cn = src:main'
+        'cnc = cnc:main'
         ],
       }
     )
